f8d49fc5ddd7d2589b182c0f28644e9e83b13ab6
Bjarne Christel <develop.christel@gmail.com>
2023-03-24 15:23:09 +0100

    fix: sql connection to database

--- system/dbconnect.php
+++ system/dbconnect.php
@@ -1,5 +1,12 @@
 <?php
     include('dbsettings.php');
-    $connection = mysql_connect($dbhost, $dbuser, $dbpassword);
-    mysql_select_db($db, $connection);
+
+    try {
+        $conn = new PDO("mysql:host=$dbhost; dbname=$db", $dbuser, $dbpassword);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connected successfully";
+    } catch(PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+    }
 ?>
